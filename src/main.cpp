39f3eb8088c1877a3d6fd456fbdc6f40c6dac237
NewBulavka <m2409446@edu.misis.ru>
2025-03-03 14:56:00 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,12 @@
+#include <iostream>
+#include "utils.cpp"
+using namespace std;
+
+int main() {
+    int x = 10;
+    int y = 5;
+    cout << add(x, y) << "\n";
+    cout << subtract(x, y) << "\n";
+    return 0;
+}
 
